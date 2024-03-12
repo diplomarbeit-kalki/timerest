5a5d924cc8141128c3b1f8608ba807f7f5a58ff5
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-03-12 21:07:59 +0100

    Updated GET media/porfilepicture/withPsnr Route

--- src/controllers/media/getMedia.ts
+++ src/controllers/media/getMedia.ts
@@ -12,14 +12,22 @@ export async function getProfilepictureByPsnr(req: any, res: any) {
 
     try {
         const result = await db.collection('employees').findOne({ psnr: parsedPsnr });
+        const altBildPfad = path.join(__dirname, `../../../public/profilepictures/placeholder.webp`);
 
         if (!result) {
-            return res.status(400).json({ message: 'Employee not found' });
+            if (psnr === "placeholder") {
+                res.status(200).sendFile(altBildPfad);
+                return;
+            }
+            else {
+                return res.status(400).json({ message: 'Employee not found' });
+            }
         }
+
         // Überprüfe, ob die Bilddatei existiert
 
         const bildPfad = path.join(__dirname, `../../../public/profilepictures/${result.profilepicture}.webp`);
-        const altBildPfad = path.join(__dirname, `../../../public/profilepictures/placeholder.webp`);
+       
 
         fs.exists(bildPfad, exists => {
             if (exists) {
