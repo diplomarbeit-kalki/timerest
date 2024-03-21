7fb479319e64f0812b774e3d50a5ad10a5cc23bc
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-03-21 16:51:26 +0100

    Updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import DatabaseClient from './database/client';
 import * as http from 'http';
 import * as WebSocket from 'ws';
-
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
-
-const client = new DatabaseClient();
+const dbclient = new DatabaseClient();
 const webSocketConnections: WebSocket[] = [];
-
 const restPort = 3001;
 const websocketPort = 3002;
 
@@ -19,12 +16,13 @@ async function start() {
 
     const app = express();
     app.use(cors());
-
-    await client.connectToDatabase();
-    console.log("index---Datenbankverbindung aufgebaut");
-    app.db = client.getDatabase();
-    app.webSocketConnections = webSocketConnections;
     app.use(express.json());
+
+    await dbclient.connectToDatabase();
+
+    app.db = dbclient.getDatabase();
+    app.webSocketConnections = webSocketConnections;
+
 
     // Routes
     app.use('/employees', require('./routes/employees'));
@@ -36,13 +34,13 @@ async function start() {
     app.use('/media', require('./routes/media'));
     app.use('/cards', require('./routes/cards'));
 
-    //Websocket
+    // Websocket
     wss.on('connection', (ws) => {
       console.log('Websocket---Client connected');
       webSocketConnections.push(ws);
 
       // Senden einer Nachricht an den Client, wenn die Verbindung hergestellt ist
-      ws.send(JSON.stringify({message: 'Verbindung mit Server hergestellt!'}));
+      ws.send(JSON.stringify({ message: 'Verbindung mit Server hergestellt!' }));
 
       // Ereignishandler für eingehende Nachrichten vom Client
       ws.on('message', (message) => {
@@ -52,17 +50,23 @@ async function start() {
       // Ereignishandler für das Schließen der Verbindung
       ws.on('close', () => {
         console.log('Websocket---Client disconnected');
+        // Entfernen der Verbindung aus dem Array
+        const index = webSocketConnections.indexOf(ws);
+        if (index !== -1) {
+          webSocketConnections.splice(index, 1);
+          console.log('Websocket---Clientverbindung entfernt');
+        }
       });
     });
 
     // Starten den Websocket Server
     server.listen(websocketPort, () => {
-      console.log(`index---WebSocket-Server läuft auf Port ${websocketPort}`);
+      console.log(`app---WebSocket-Server läuft auf Port ${websocketPort}`);
     });
 
     // Starten den REST Server
     app.listen(restPort, () => {
-      console.log(`index---REST-Server läuft auf Port ${restPort}`);
+      console.log(`app---REST-Server läuft auf Port ${restPort}`);
     });
   }
   catch (error) {
